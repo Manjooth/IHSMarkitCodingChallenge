45c77c904c001b2fe7994e1643fc92895a2119c3
Manjooth <manjoothkler@gmail.com>
2021-12-20 21:15:33 +0000

    updating interface with comments

--- src/main/java/BookingManagerInterface.java
+++ src/main/java/BookingManagerInterface.java
@@ -3,7 +3,18 @@ import java.time.LocalDate;
 public interface BookingManagerInterface
 {
 
+    /**
+    @return true if there is no booking for the given room on the date, otherwise false
+    **/
     public boolean isRoomAvailable(Integer room, LocalDate date);
+
+    /**
+    Add a booking for the given guest in the given room on the given date. If the room is not available, throw a suitable Exception.
+    **/
     public void addBooking(String guest, Integer room, LocalDate date);
+
+    /**
+    @return a list of all the available room numbers for the given date
+    **/
     public Iterable<Integer> getAvailableRooms(LocalDate date);
 }
